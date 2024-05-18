1c5088f7c34dec9bf8b58263614b6069ea4807be
pyronn <pyronn@163.com>
2024-05-18 17:28:30 +0800

    add: suppor i18n

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,8 +1,12 @@
+import {useTranslations} from "next-intl";
+
 export default function Home() {
+    const h = useTranslations('Home')
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
           hello world
+          <h1>{h('title')}</h1>
       </div>
     </main>
   );
