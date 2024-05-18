35287fa2d6e5eeecb1890ae52b0237920b60adae
pyronn <pyronn@163.com>
2024-05-18 17:51:15 +0800

    feat: add theme switcher

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,4 +1,6 @@
 import {useTranslations} from "next-intl";
+import {ThemeSwitcher} from "@/components/ThemeSwitcher";
+import {Button} from "@nextui-org/button";
 
 export default function Home() {
     const h = useTranslations('Home')
@@ -6,7 +8,9 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
           hello world
+          <Button>click me</Button>
           <h1>{h('title')}</h1>
+          <ThemeSwitcher />
       </div>
     </main>
   );
